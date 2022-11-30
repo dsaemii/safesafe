b35e23268a22c6933be5144f87e0428daaa3fce3
Samira Stragiotti <46267818+dsaemii@users.noreply.github.com>
2022-11-30 13:40:06 +0100

    Initial commit

---
+++ src/test/java/com/example/safesafe/SafesafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.safesafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafesafeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
