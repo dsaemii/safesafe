2540831cf58fb9a04925908fb71adc1a6b75ec9f
Samira Stragiotti <46267818+dsaemii@users.noreply.github.com>
2022-12-07 15:21:42 +0100

    Safer

--- src/main/java/ch/stragiotti/safersafe/SafersafeRestController.java
+++ src/main/java/ch/stragiotti/safersafe/SafersafeRestController.java
@@ -3,6 +3,7 @@ package ch.stragiotti.safersafe;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.stream.Collectors;
@@ -15,14 +16,13 @@ public class SafersafeRestController {
     @Autowired
     private PasswordRepository safersafeRepository;
 
-    @GetMapping("token")
-    public String getToken() {
-
-        return StreamSupport.stream(
-                        jokeRepository
-                                .findAll()
-                                .spliterator(), false)
-                .collect(Collectors.toList());
+    @GetMapping("login")
+    public String login(@RequestBody String username, @RequestBody String password) {
+        String token;
+        token = StreamSupport.stream(safersafeRepository
+                .findAll()
+        )
+        PasswordRepository.stream
     }
 
 }
