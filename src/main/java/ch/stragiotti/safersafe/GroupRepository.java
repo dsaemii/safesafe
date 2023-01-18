68562de2179372951fdaadd5820eacd341ac1c09
totoluto <94611749+totoluto@users.noreply.github.com>
2023-01-18 15:44:18 +0100

    deleted wrong files

--- src/main/java/ch/stragiotti/safersafe/GroupRepository.java
+++
@@ -1,8 +0,0 @@
-package ch.stragiotti.safersafe;
-
-import org.springframework.data.repository.CrudRepository; // CRUD - create, read, update, delete
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GroupRepository extends CrudRepository<Group, Long> {
-}
